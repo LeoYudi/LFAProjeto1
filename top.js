c0bd0b662242068c3c1f8d7be1b93ea25f6570a9
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-08-13 10:40:35 -0300

    sem console.log

--- top.js
+++ top.js
@@ -1,8 +1,6 @@
 function testInfo(phoneInput, regexInput) {
   var re = RegExp(regexInput.value);
   var OK = re.exec(phoneInput.value);
-  console.log(re);
-  console.log(OK);
   if (OK)
     window.alert("tah certo");
   else
