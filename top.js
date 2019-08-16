d15e5792e02bf0ff6e45702543ab94972161edae
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-08-16 16:19:50 -0300

    mt coisa

--- top.js
+++ top.js
@@ -1,8 +1,37 @@
-function testInfo(phoneInput, regexInput) {
-  var re = RegExp(regexInput.value);
-  var OK = re.exec(phoneInput.value);
+function testInfo(texto, regexInput) {
+  var re = RegExp(`${regexInput.value}`);
+  var OK = re.exec(texto.value);
   if (OK)
     window.alert("tah certo");
   else
     window.alert("tah errado");
 }
+
+var nodes = new vis.DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+  { id: 5, label: 'Node 5' }
+]);
+
+// create an array with edges
+var edges = new vis.DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 }
+]);
+
+// create a network
+var container = document.getElementById('mynetwork');
+
+// provide the data in the vis format
+var data = {
+  nodes: nodes,
+  edges: edges
+};
+var options = {};
+
+// initialize your network!
+var network = new vis.Network(container, data, options);
