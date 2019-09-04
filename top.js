e2503d53777140c383e2bbe6a0e53239d7313f8f
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-09-04 11:12:11 -0300

    deu certo caralho

--- top.js
+++ top.js
@@ -12,31 +12,61 @@ function testInfo(texto, regexInput) {
 
 
 
-var nodes = new vis.DataSet([
-  { id: 1, label: 'Node 1' },
-  { id: 2, label: 'Node 2' },
-  { id: 3, label: 'Node 3' },
-  { id: 4, label: 'Node 4' },
-  { id: 5, label: 'Node 5' }
+// var nodes = new vis.DataSet([
+//   { id: 1, label: 'Node 1' },
+//   { id: 2, label: 'Node 2' },
+//   { id: 3, label: 'Node 3' },
+//   { id: 4, label: 'Node 4' },
+//   { id: 5, label: 'Node 5' }
+// ]);
+
+// // create an array with edges
+// var edges = new vis.DataSet([
+//   { from: 1, to: 3 },
+//   { from: 1, to: 2 },
+//   { from: 2, to: 4 },
+//   { from: 2, to: 5 }
+// ]);
+
+// // create a network
+// var container = document.getElementById('mynetwork');
+
+// // provide the data in the vis format
+// var data = {
+//   nodes: nodes,
+//   edges: edges
+// };
+// var options = {};
+
+// // initialize your network!
+// var network = new vis.Network(container, data, options);
+
+
+var container = document.getElementById('mynetwork');
+var nodes = [], edges = [], graph;
+nodes = new vis.DataSet();
+nodes.add([
+  { id: '1', label: 'Node 1' },
+  { id: '2', label: 'Node 2' },
+  { id: '3', label: 'Node 3' },
+  { id: '4', label: 'Node 4' },
+  { id: '5', label: 'Node 5' }
 ]);
 
 // create an array with edges
-var edges = new vis.DataSet([
-  { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-  { from: 2, to: 5 }
+edges = new vis.DataSet();
+
+edges.add([
+  { id: '1', from: '1', to: '2' },
+  { id: '2', from: '1', to: '3' },
+  { id: '3', from: '2', to: '4' },
+  { id: '4', from: '2', to: '5' }
 ]);
 
-// create a network
-var container = document.getElementById('mynetwork');
-
-// provide the data in the vis format
+// create a graph
 var data = {
   nodes: nodes,
   edges: edges
 };
 var options = {};
-
-// initialize your network!
-var network = new vis.Network(container, data, options);
+graph = new vis.Graph(container, data, options);
