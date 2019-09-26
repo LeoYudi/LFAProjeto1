37bc5c69191633aedbfacdca08030d407dd88021
Leonardo Yudi <lol_leonardohiguti@outlook.com>
2019-09-26 14:38:41 -0300

    tirando toda parte de automatos

--- top.js
+++ top.js
@@ -104,32 +104,3 @@ function pegarGramatica() {
   }
   return gramatica;
 }
-
-var container = document.getElementById('mynetwork');
-var nodes = [], edges = [], graph;
-nodes = new vis.DataSet();
-nodes.add([
-  { id: '1', label: 'Node 1' },
-  { id: '2', label: 'Node 2' },
-  { id: '3', label: 'Node 3' },
-  { id: '4', label: 'Node 4' },
-  { id: '5', label: 'Node 5' }
-]);
-
-// create an array with edges
-edges = new vis.DataSet();
-
-edges.add([
-  { id: '1', from: '1', to: '2' },
-  { id: '2', from: '1', to: '3' },
-  { id: '3', from: '2', to: '4' },
-  { id: '4', from: '2', to: '5' }
-]);
-
-// create a graph
-var data = {
-  nodes: nodes,
-  edges: edges
-};
-var options = {};
-graph = new vis.Graph(container, data, options);
